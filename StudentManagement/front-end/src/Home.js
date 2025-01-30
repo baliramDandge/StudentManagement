4a6d77ce9e781b576dc0b21ae4ce626b8c32d134
baliramDandge <124309830+baliramDandge@users.noreply.github.com>
2025-01-30 20:52:22 +0530

    Add files via upload

---
+++ StudentManagement/front-end/src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+	return (
+		<div className="home-container">
+			<div className="welcome-banner">
+				<h2>Welcome to the Student Management System</h2>
+			</div>
+
+			
+		</div>
+	);
+};
+
+export default Home;
